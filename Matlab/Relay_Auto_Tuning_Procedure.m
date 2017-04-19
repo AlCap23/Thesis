67229e76d72490883d3fb085c699b5272184ebfa
AlCap23 <julius.martensen@gmail.com>
2017-04-19 09:31:50 +0200

    Corrected Relay_Auto_Tuning_Procedure and the Simulink Model. Exported Simulink Model to previous Version.
    Signed-off-by: AlCap23 <julius.martensen@gmail.com>

--- Matlab/Relay_Auto_Tuning_Procedure.m
+++ Matlab/Relay_Auto_Tuning_Procedure.m
@@ -9,16 +9,17 @@ clc
 
 % Simulation
 t_start = 0; % Start Time in s
-t_end = 200; % Stop Time in s
+t_end = 1000; % Stop Time in s
+dt = 0.1; % Time Step in s
 
 % Plant Parameter
-Num = [10]; % Numerator of the Transfer Function
-Den = [10,1]; % Denumerator of the Transfer Function
+Num = [10,5]; % Numerator of the Transfer Function
+Den = [10,10,10,1]; % Denumerator of the Transfer Function
 L = 9; % Time Delay of the Transfer Function
 
 % Tuning Parameters
-y0 = 10; % Set Point / Working Point
 u0 = 2 ; % Constant Input on the Plant
+y0 = dcgain(tf(Num,Den))*u0; % Set Point / Working Point
 h = 0.1; % Hysterisis for Relay
 d1 = 5; % Upper Limit of the Output
 d2 = 2; % Lower Limit of the Output
@@ -28,16 +29,6 @@ d2 = 2; % Lower Limit of the Output
 load_system('Relay_Auto_Tuning.slx');
 sim('Relay_Auto_Tuning.slx');
 
-%% Postprocess Data
-
-% figure()
-% plot(u)
-% hold on
-% plot(y)
-% grid on
-% xlabel('Time [s]');
-% ylabel('Input and Output');
-% title('Relay Autotuning');
 
 %% Get the Plant Parameter
 % Take sample from the middle of the Test
@@ -48,45 +39,43 @@ time_Data = y.Time(time_limit:end); % Get the Time Sample
 sample_rate = time_Data(2)-time_Data(1); % Assume constant sample rate
 
 
-% Get the on and off time
-lower_limit = round(length(y_Data)/4)
-upper_limit =  round(length(y_Data)*3/4)
-% Find first minimum of u and minimum of y before that
-[y_min, pos_y_min] = min(y_Data( lower_limit : 2*lower_limit ) )
-pos_y_min = pos_y_min+round(length(y_Data)/4)
-[u_min, pos_u_min] = min(u_Data(pos_y_min+1:upper_limit))
-pos_u_min = pos_u_min + pos_y_min
-% Find first maximum of u and minimum of y since u_min and u_max
-[y_max, pos_y_max] = max(y_Data(pos_y_min:upper_limit)); % This order is more stable
-pos_y_max = pos_y_max + pos_y_min
-[u_max, pos_u_max] = max(u_Data(pos_y_max:upper_limit));
-pos_u_max = pos_u_max+pos_y_max
-% Calculate off time
-t_off = time_Data(pos_u_max)-time_Data(pos_u_min)
-% Calculate the first half of the integral output
-%I_u = sum(u_Data(pos_u_min:pos_u_max))*sample_rate;
-I_y = sum(y_Data(pos_y_min:pos_y_max)-y0)*sample_rate;
-% Get the next minimum
-[y_min,pos_y_min] = min(y_Data(pos_u_max:upper_limit));
-pos_y_min = pos_y_min+pos_u_max
-[u_min, pos_u_min] = min(u_Data(pos_y_min:upper_limit));
-pos_u_min = pos_u_min+pos_y_min
-% Get the on time
-t_on = time_Data(pos_u_min)-time_Data(pos_u_max)
+% Find the Peaks in the Data
+[ypeaks, ypeakloc] = findpeaks(y_Data); % Get the location and the value of the peaks
+[upeaks, upeakloc] = findpeaks(u_Data);
 
-% Calculate the second half of the integral output
-%I_u = I_u + sum(u_Data(pos_u_max:pos_u_min))*sample_rate;
-I_y = I_y + sum(y_Data(pos_y_max:pos_y_min)-y0)*sample_rate;
-% Calculate the integral of the relay output
-I_u = abs(d1)*t_on-abs(d2)*t_off ; % Eq. 12
+% Since we know that we got a oscillation, use that knowledge to find first
+% minimum
+[u_Min, loc_u_Min] = min(u_Data(upeakloc(1):upeakloc(2)));
+loc_u_Min = loc_u_Min + upeakloc(1);
 
+% Get the on time from first maximum to minimum of u
+t_on = time_Data(loc_u_Min) - time_Data(upeakloc(1));
+% Get the off time from minimum to second maximum
+t_off = time_Data(upeakloc(2)) - time_Data(loc_u_Min);
+
+% Get the numerical integral of the plant process output
+I_y = sum(y_Data(ypeakloc(1):ypeakloc(2))-y0)*sample_rate;
+% Get the integral action of the input
+I_u = (upeaks(1)-u0)*t_on + (u_Min-u0)*t_off;
+% Static Gain
+K_P = I_y/I_u
 % Calculate the normalized time , Time Constant and Delay
 rho = max(t_on,t_off)/min(t_on,t_off); % Half Period as given in Eq. 10
 gamma = max(d1,d2) / min(d1,d2); % Asymetry level, Eq. 7
-tau = gamma-rho / (gamma-1) / (0.35*rho + 0.65); %Normalized Time, Eq.9
-
-% Static Gain
-K_P = I_y/I_u
+tau = (gamma-rho) / (gamma-1) / (0.35*rho + 0.65); %Normalized Time, Eq.9
 % Time Constant
-T_on = t_on / log( (h*abs(K_P)-d2+exp(tau/(1-tau))*(d1+d2)) / (d1-h*abs(K_P)) )
-T_off = t_off / log( (h*abs(K_P)-d1+exp(tau/(1-tau))*(d1+d2)) / (d2-h*abs(K_P)) )
+T_on = t_on / log( (h/abs(K_P)-d2+exp(tau/(1-tau))*(d1+d2)) / (d1-h/abs(K_P)) );
+T_off = t_off / log( (h/abs(K_P)-d1+exp(tau/(1-tau))*(d1+d2)) / (d2-h/abs(K_P)) );
+T = 1/2*(T_on+T_off)
+L = T* (tau/(1-tau))
+
+%% Postprocess Data
+
+figure()
+plot(u)
+hold on
+plot(y)
+grid on
+xlabel('Time [s]');
+ylabel('Input and Output');
+title('Relay Autotuning');
